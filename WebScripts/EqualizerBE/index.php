3997d94c89112c0690d57f88920d071ed6612233
the_cowboy <ravimohan1991@gmail.com>
2021-07-10 23:07:17 +0530

    PHP environment set

---
+++ WebScripts/EqualizerBE/index.php
@@ -0,0 +1,8 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>My first PHP script</title>
+  </head>
+  <body>
+    <p><?php echo "I am The_Cowboy. Who are you?";?></p>
+  </body>
+</html>
