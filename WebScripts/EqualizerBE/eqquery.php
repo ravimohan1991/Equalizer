fda92f6042687f63eb41735559cdbe76ad2dd50b
the_cowboy <ravimohan1991@gmail.com>
2021-07-12 18:43:41 +0530

    Added relevant license to the PHP scripts

--- WebScripts/EqualizerBE/eqquery.php
+++ WebScripts/EqualizerBE/eqquery.php
@@ -1,9 +1,28 @@
 <?php
-header("Content-Type: text/html; charset=utf-8");
+/*
+ *   --------------------------
+ *  |  eqquery.php
+ *   --------------------------
+ *   This file is part of Equalizer for UT2004.
+ *
+ *   Equalizer is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Equalizer is distributed in the hope and belief that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Equalizer.  if not, see <https://www.gnu.org/licenses/>.
+ *
+ *   Timeline:
+ *   May, 2021: First inscription
+ */
 
-/******************************************************************************
- *	
- ******************************************************************************/
+header("Content-Type: text/html; charset=utf-8");
 
 error_reporting(E_ALL);
 include_once 'main.php';
--- WebScripts/EqualizerBE/main.php
+++ WebScripts/EqualizerBE/main.php
@@ -1,4 +1,27 @@
 <?php
+/*
+ *   --------------------------
+ *  |  main.php
+ *   --------------------------
+ *   This file is part of Equalizer for UT2004.
+ *
+ *   Equalizer is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Equalizer is distributed in the hope and belief that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Equalizer.  if not, see <https://www.gnu.org/licenses/>.
+ *
+ *   Timeline:
+ *   May, 2021: First inscription
+ */
+
 $servername = "localhost";
 $username = "cowboy";
 $password = "BWxFPjNuIYBPxxg0";
