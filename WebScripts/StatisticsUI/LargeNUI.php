724fbe4253ab0f277b0b3ebbc082874128d1013c
the_cowboy <ravimohan1991@gmail.com>
2022-02-12 04:02:08 +0530

    Good enough UI for backend database

---
+++ WebScripts/StatisticsUI/LargeNUI.php
@@ -0,0 +1,119 @@
+<?php
+/*
+ *   --------------------------
+ *  |  eqquery.php
+ *   --------------------------
+ *   This file is part of Equalizer for UT2004.
+ *
+ *   Equalizer is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Equalizer is distributed in the hope and belief that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Equalizer.  if not, see <https://www.gnu.org/licenses/>.
+ *
+ *   Timeline:
+ *   May, 2021: First inscription
+ */
+
+$servername = "localhost";
+$username = "cowboy";
+$password = "BWxFPjNuIYBPxxg0";
+$database = "CowboysTestDatabase";
+
+
+$mysqli = new mysqli($servername, $username, 
+                $password, $database);
+
+// Checking for connections
+if ($mysqli->connect_error)
+{
+    die('Connect Error (' . 
+    $mysqli->connect_errno . ') '. 
+    $mysqli->connect_error);
+}
+
+
+// SQL query to select data from the database
+$sqlquery = "SELECT * FROM EQPlayerInformation ORDER BY TimePlayedMinutes DESC";
+$result = $mysqli->query($sqlquery);
+$mysqli->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <meta charset="UTF-8">
+    <title>GFG User Details</title>
+    <!-- CSS FOR STYLING THE PAGE -->
+    <style>
+        table {
+            margin: 0 auto;
+            font-size: large;
+            border: 1px solid black;
+        }
+  
+        h1 {
+            text-align: center;
+            color: #006600;
+            font-size: xx-large;
+            font-family: 'Gill Sans', 'Gill Sans MT', 
+            ' Calibri', 'Trebuchet MS', 'sans-serif';
+        }
+  
+        td {
+            background-color: #E4F5D4;
+            border: 1px solid black;
+        }
+  
+        th,
+        td {
+            font-weight: bold;
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+        }
+  
+        td {
+            font-weight: lighter;
+        }
+    </style>
+</head>
+  
+<body>
+    <section>
+        <h1>Large Player Statistics</h1>
+        <!-- TABLE CONSTRUCTION-->
+        <table>
+            <tr>
+                <th>Player</th>
+                <th>Time Played (Minutes)</th>
+                <th>Grand Total</th>
+            </tr>
+            <!-- PHP CODE TO FETCH DATA FROM ROWS-->
+            <?php   // LOOP TILL END OF DATA 
+                while($rows = $result->fetch_assoc())
+                {
+             ?>
+            <tr>
+                <!--FETCHING DATA FROM EACH 
+                    ROW OF EVERY COLUMN-->
+                <td><?php echo $rows['EQName'];?></td>
+                <td><?php echo $rows['TimePlayedMinutes'];?></td>
+                <td><?php echo $rows['Points'];?></td>
+            </tr>
+            <?php
+                }
+             ?>
+        </table>
+    </section>
+</body>
+  
+</html>
